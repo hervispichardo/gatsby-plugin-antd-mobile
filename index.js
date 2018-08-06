1221964db250366072a8fa7a45977641215559ef
Hervis Pichardo <hervis15@gmail.com>
2018-08-06 17:38:48 -0400

    build

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// noop
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.modifyBabelrc = function (_ref, _ref2) {
+  var babelrc = _ref.babelrc;
+  var style = _ref2.style;
+
+  return _extends({}, babelrc, {
+    plugins: babelrc.plugins.concat([['import', {
+      libraryName: 'antd-mobile',
+      style: style ? style : 'css'
+    }]])
+  });
+};
